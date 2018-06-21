2d7838c0acee442b8bf1a9e9ac48297f78e33475
Lowdingg <lowdinggg@gmail.com>
2018-06-21 14:55:16 +0100

    Set up the JavaFX stuff. Added JFoenix and Icons

--- src/main/java/com/gitlab/morpion/Main.java
+++ src/main/java/com/gitlab/morpion/Main.java
@@ -1,9 +1,23 @@
 package com.gitlab.morpion;
 
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application{
 
     public static void main(String[] args) {
-
+        launch(args);
     }
 
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/jfx/design.fxml"));
+        primaryStage.setTitle("Morpion");
+        primaryStage.setResizable(false);
+        primaryStage.setScene(new Scene(root, 600, 370));
+        primaryStage.show();
+    }
 }
---
+++ src/main/java/com/gitlab/morpion/jfx/Controller.java
@@ -0,0 +1,5 @@
+package com.gitlab.morpion.jfx;
+
+public class Controller {
+
+}
